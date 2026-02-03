1aa729892c8ba13c2d63260d9ad76b7ec35a9c79
Michael J. Seiter <78192+michaeljseiter@users.noreply.github.com>
2026-02-02 23:29:12 -0600

    Begin validation for Profile/Links

--- internal/profile.go
+++ internal/profile.go
@@ -1,5 +1,46 @@
 package profile
 
+import (
+	"errors"
+	"fmt"
+	"net/url"
+)
+
+// Validate checks if the profile is valid
+func (p *Profile) Validate() error {
+	if p.Name == "" {
+		return errors.New("name is required and must not empty")
+	}
+
+	if len(p.Links) == 0 {
+		return errors.New("links must be a not-empty array")
+	}
+
+	for i, link := range p.Links {
+		if err := link.Validate(); err != nil {
+			return fmt.Errorf("link[%d]: %w", i, err)
+		}
+	}
+
+	return nil
+}
+
+// Validate check if a link is valid
+func (l *Link) Validate() error {
+	if l.Label == "" {
+		return errors.New("label is required and must not be empty")
+	}
+
+	if l.URL == "" {
+		return errors.New("url is required and must not be empty")
+	}
+
+	parsedURL, err := url.Parse(l.URL)
+	if err != nil {
+		return fmt.Errorf("invaid url: %w", err)
+	}
+}
+
 // Profile represents a clinks profile
 type Profile struct {
 	Name  string `json:"name"`
